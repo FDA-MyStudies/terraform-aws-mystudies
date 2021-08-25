b9e107024710d4b1ff4b4ed53636bd3c276e0de9
Stuart MacDonald <32204841+labkey-stuartm@users.noreply.github.com>
2021-08-25 16:41:07 -0700

    intial vpc config with alb and default sec groups

--- examples/sample-deployment/main.tf
+++ examples/sample-deployment/main.tf
@@ -1,6 +1,10 @@
+
+
 provider "aws" {
   region = local.region
 }
+
+
 
 locals {
   region = "us-west-2"
@@ -18,4 +22,15 @@ module "mystudies" {
   private_key_path   = var.private_key_path
   security_group_ids = var.security_group_ids
   subnet_id          = var.subnet_id
+  formation          = var.formation
+  formation_type     = var.formation_type
+  common_tags        = var.common_tags
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+  vpc_cidr           = var.vpc_cidr
+  office_cidr_A      = var.office_cidr_A
+  office_cidr_B      = var.office_cidr_B
+  alb_ssl_cert_arn   = var.alb_ssl_cert_arn
+  alb_ssl_policy     = var.alb_ssl_policy
+
 }
