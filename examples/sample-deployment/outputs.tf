0245a46e350001380c09eb3769d201cd0d5bfae6
labkey-stuartm <32204841+labkey-stuartm@users.noreply.github.com>
2021-08-30 09:02:12 -0700

    intial vpc config with alb and default sec groups (#3)
    * intial vpc config with alb and default sec groups
    
    * Fix actions and favor terrascan for security (#5)
    
    * fix actions file extension
    
    * use tf 1.0.5 in actions
    
    * allow action to be run manually/on push
    
    * move tfsec/terrascan farther down the actions yml
    
    * use tfsec PR commenter instead
    
    * disable terrascan serif upload
    
    * enable terrascan w/o sarif file
    
    * ensure code is checked out for terrascan/tfsec
    
    * comment out tfsec for now
    
    * intial vpc config with alb and default sec groups
    
    * outputs.tf for sample-deployment
    
    * remove empty scehdule tag from common tags
    
    * add example s3 backend state config
    
    * update alb module from 6.0.0 to 6.5.0
    
    Co-authored-by: Isa Farnik <isaf@labkey.com>

---
+++ examples/sample-deployment/outputs.tf
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2021 LabKey Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+output "vpc_id" {
+  value = module.mystudies.vpc_id
+}
+
+output "vpc_arn" {
+  value = module.mystudies.vpc_arn
+}
+
+output "vpc_cidr" {
+  value = module.mystudies.vpc_cidr
+}
+
+output "igw_id" {
+  value = module.mystudies.igw_id
+}
+
+output "vpc_alb_arn" {
+  value = module.mystudies.vpc_alb_arn
+}
+
+output "base_domain" {
+  value = var.base_domain
+}
+
--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@
 #
 
 terraform {
+
   required_version = "~> 1"
 
   required_providers {
@@ -25,11 +26,12 @@ terraform {
       version = ">= 3.55.0"
     }
   }
+  # Note: TF State backend configured via state.tf.template
 }
 
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 locals {
   # vpc_id             = var.vpc_id == null ? module.vpc.vpc_id : var.vpc_id
@@ -39,11 +41,14 @@ locals {
 
 
 
-  name_prefix = "mystudies"
-
-  additional_tags = {
-    Prefix = local.name_prefix
-  }
+  name_prefix = var.formation
+  name_type   = var.formation_type
+  env_dns     = var.base_domain
+  additional_tags = merge(var.common_tags, tomap({
+    Prefix      = local.name_prefix
+    Environment = var.formation_type
+    Formation   = var.formation
+  }))
 
   instance_type = "t3a.large"
 
@@ -61,62 +66,224 @@ locals {
 #
 ###
 
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = ">= 3.6.0"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = ">= 3.6.0"
 
-#   create_vpc = var.vpc_id == null
+  #create_vpc = var.vpc_id == null
 
-#   name = "${local.name_prefix}-vpc"
-#   cidr = var.cidr
+  name = "${local.name_prefix}-vpc"
+  cidr = var.vpc_cidr
 
-#   azs = [
-#     data.aws_availability_zones.available.names[0],
-#     data.aws_availability_zones.available.names[1],
-#   ]
+  azs = [
+    data.aws_availability_zones.available.names[0],
+    data.aws_availability_zones.available.names[1],
+    data.aws_availability_zones.available.names[2],
+  ]
 
-#   private_subnets = var.private_subnets
-#   public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
-#   enable_nat_gateway = true
-#   single_nat_gateway = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
-#   # required for resolution of EFS mount hostname
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
+  # Default security group - ingress/egress rules cleared to deny all
+  manage_default_security_group  = true
+  default_security_group_ingress = []
+  default_security_group_egress  = []
 
-#   tags = merge(
-#     local.additional_tags,
-#     {
-#       Name = "${local.name_prefix}-vpc"
-#     },
-#   )
-# }
+  # required for resolution of EFS mount hostname
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-# module "ssh_sg" {
-#   source  = "terraform-aws-modules/security-group/aws//modules/ssh"
-#   version = ">= 4.3.0"
+  tags = merge(
+    local.additional_tags,
+    {
+      Name = "${local.name_prefix}-${local.name_type}-vpc"
+    },
+  )
+}
 
-#   # skip creation if user has supplied own security groups list
-#   create = local.create_sg
+module "endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = ">= 3.0.0"
 
-#   name = "${local.name_prefix}-ssh-sg"
-#   # vpc_id = module.vpc.vpc_id
-#   vpc_id = var.vpc_id
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-#   description = "Security group with SSH ports open for office/VPN (IPv4 CIDRs)"
+  security_group_ids = [
+    module.https_private_sg.security_group_id,
+    module.office_ssh_sg.security_group_id,
+  ]
 
-#   ingress_cidr_blocks = [
-#     "0.0.0.0"
-#   ]
+  endpoints = {
+    ssm = {
+      service             = "ssm"
+      private_dns_enabled = true
+      tags = merge(
+        local.additional_tags,
+        {
+          Name = "${local.name_prefix}-${local.name_type}-ssm-endpoint"
+        },
+      )
+    },
+  }
 
-#   tags = merge(
-#     local.additional_tags,
-#     {
-#       Name = "${local.name_prefix}-ssh-sg"
-#     },
-#   )
-# }
+  tags = local.additional_tags
+}
+
+module "lb_http_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = ">= 4.3.0"
+
+  name        = "${var.formation}-${var.formation_type}-alb-http"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security group with HTTP ports open for everybody (IPv4 CIDR), egress ports are all world open"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = local.additional_tags
+}
+
+module "lb_https_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/https-443"
+  version = ">= 4.3.0"
+
+  name        = "${var.formation}-${var.formation_type}-alb-https"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security group with HTTPS ports open for everybody (IPv4 CIDR), egress ports are all world open"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = local.additional_tags
+}
+
+module "https_private_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/https-443"
+  version = ">= 4.3.0"
+
+  name        = "${var.formation}-${var.formation_type}-private-https"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security group with HTTPS ports open for private VPC subnets"
+
+  ingress_cidr_blocks = var.private_subnets
+
+  tags = local.additional_tags
+}
+
+module "office_ssh_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = ">= 4.3.0"
+
+  # skip creation if user has supplied own security groups list
+  #create = local.create_sg
+
+  name   = "${local.name_prefix}-ssh-sg"
+  vpc_id = module.vpc.vpc_id
+  #  vpc_id = var.vpc_id
+
+  description = "Security group with SSH ports open for office/VPN (IPv4 CIDRs)"
+
+  ingress_cidr_blocks = tolist([var.office_cidr_A, var.office_cidr_B])
+
+  tags = merge(
+    local.additional_tags,
+    {
+      Name = "${local.name_prefix}-ssh-sg"
+    },
+  )
+}
+
+
+# use existing DNS Zone provided by var.base.domain
+data "aws_route53_zone" "env_zone" {
+  name = var.base_domain
+}
+
+
+
+/* Example to create ACM vs use supplied existing certificate
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = ">= 3.2.0"
+
+  domain_name = "*.${local.env_dns}"
+  zone_id     = data.aws_route53_zone.env_zone.id
+
+  validation_method    = "DNS"
+  validate_certificate = true
+  wait_for_validation  = false
+
+  subject_alternative_names = var.formation_type == "prod" ? ["*.${var.base_domain}"] : []
+
+  tags = local.additional_tags
+}
+*/
+
+
+
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = ">= 6.5.0"
+
+  depends_on = [
+    module.vpc,
+  ]
+
+  name = "${var.formation}-vpc-${var.formation_type}-alb"
+
+  load_balancer_type = "application"
+
+  idle_timeout = 600
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  security_groups = [
+    module.lb_http_sg.security_group_id,
+    module.lb_https_sg.security_group_id,
+  ]
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "redirect"
+      redirect = {
+        port        = 443
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  ]
+
+  tags = local.additional_tags
+}
+
+resource "aws_alb_target_group" "default_target" {
+  name     = "${var.formation_type}-vpc-${var.formation}-default"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = module.vpc.vpc_id
+
+  tags = local.additional_tags
+}
+
+resource "aws_alb_listener" "alb_https_listener" {
+  load_balancer_arn = module.alb.lb_arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = var.alb_ssl_policy
+  certificate_arn   = var.alb_ssl_cert_arn
+
+  default_action {
+    target_group_arn = aws_alb_target_group.default_target.arn
+    type             = "forward"
+  }
+}
+
 
 # locals {
 #   security_groups = local.create_sg ? [module.ssh_sg.security_group_id] : var.security_groups
@@ -146,6 +313,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+/*
 resource "aws_instance" "response" {
 
   ami           = data.aws_ami.ubuntu.id
@@ -210,6 +378,7 @@ resource "aws_instance" "response" {
     ]
   }
 }
+*/
 
 # resource "aws_instance" "registration" {
 
@@ -256,3 +425,5 @@ resource "aws_instance" "response" {
 #     EOT
 #   }
 # }
+
+
